cbdb3cbe7df9dbd189f2aed58d32aa9c7c3518e6
rmn-dev7 <coding.347@gmail.com>
2024-08-16 19:36:48 +0530

    main b dev 2

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
         for (int i = 1; i <= 6; i++) {
-            person p = new person();
-            p.setIds(String.valueOf(i));
+            System.out.println(i+89);
         }
     }
 }
