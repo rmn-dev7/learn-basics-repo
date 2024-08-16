3f8d16a03712515934996f84fa79bf96e5372735
rmn-dev7 <coding.347@gmail.com>
2024-08-16 19:30:04 +0530

    main b dev 1

--- src/demo/person.java
+++ src/demo/person.java
@@ -6,4 +6,37 @@ public class person {
     String address;
     String phoneNo;
 
+    public String getIds() {
+        return ids;
+    }
+
+    public void setIds(String ids) {
+        this.ids = ids;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+
 }
